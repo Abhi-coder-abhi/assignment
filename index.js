a3be8ba0129321311d59404b6fd6e49747f55454
Abhishek Tiwari <a.tiwari@digicust.com>
2023-11-08 21:33:13 +0530

    commit

--- index.js
+++ index.js
@@ -14,14 +14,8 @@ cloudinary.config({
   });
 
 
-  const corsOptions = {
-    origin: 'http://127.0.0.1:5173', 
-    credentials: true,
-  };
-
 app.use(express.json());
 app.use(cors());
-app.use(cors(corsOptions));
 app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Welcome to the server');
