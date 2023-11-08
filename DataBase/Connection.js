4936e7c9e8eb73762877c1995380b740eed921e6
Abhishek Tiwari <a.tiwari@digicust.com>
2023-11-08 20:25:07 +0530

    commit

--- DataBase/Connection.js
+++ DataBase/Connection.js
@@ -3,7 +3,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 //  Connection stablish with some other mongo url only for the check the dummy database
 
-mongoose.connect(MONGO_URL).then(() => {
+mongoose.connect(MONGO_URI).then(() => {
     console.log("Connected".yellow.bold);
 }).catch((error) => {
     console.log(error)
